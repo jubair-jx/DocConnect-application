23781e0db4274c7ed983523d4f59aae71ad720c8
jubair-jx <jubair.official97@gmail.com>
2024-04-07 01:23:45 +0600

    Schedule services and dateTime data fixing soon

--- src/app/modules/Schedules/schedule.services.ts
+++ src/app/modules/Schedules/schedule.services.ts
@@ -1,5 +1,24 @@
+import { addHours, format } from "date-fns";
 const createScheduleIntoDB = async (payload: any) => {
-  console.log(payload);
+  const { startDate, endDate, startTime, endTime } = payload;
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  while (currentDate <= lastDate) {
+    const startDateTime = new Date(
+      addHours(
+        `${format(currentDate, "yyyy-MM-dd")}`,
+        Number(startTime.split(":")[0])
+      )
+    );
+    const endDateTime = new Date(
+      addHours(
+        `${format(lastDate, "yyyy-MM-dd")}`,
+        Number(endTime.split(":")[0])
+      )
+    );
+    console.log(startDateTime);
+  }
 };
 
 export const ScheduleServices = {
