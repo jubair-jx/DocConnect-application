0a30581b5812a41dce0d236d8ac8e014c9fac89f
jubair-jx <jubair.official97@gmail.com>
2024-05-03 16:02:19 +0600

    hero section is completed

--- PH-Healthcare-Frontend/src/components/ui/HomePage/HeroSection.tsx
+++ PH-Healthcare-Frontend/src/components/ui/HomePage/HeroSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Image from "next/image";
 import assets from "../../../assets";
 export const HeroSection = () => {
@@ -8,6 +8,7 @@ export const HeroSection = () => {
         sx={{
           display: "flex",
           direction: "row",
+          my: 6,
         }}
       >
         <Box
@@ -25,29 +26,109 @@ export const HeroSection = () => {
             }}
           >
             <Image src={assets.svgs.grid} alt="Grid Image" />
-            <Typography variant="h2" component="h1" fontWeight={600}>
-              Healthier Hearts
-            </Typography>
-            <Typography variant="h2" component="h1" fontWeight={600}>
-              Come From
-            </Typography>
-            <Typography
-              variant="h2"
-              component="h1"
-              fontWeight={600}
-              color="primary.main"
-            >
-              Preventive Care
-            </Typography>
-            <Typography sx={{ my: 4 }}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit
-              eum iusto consequatur eius, doloribus nesciunt facere aliquid
-              eveniet et. Rerum maiores saepe cupiditate repellat recusandae
-              atque sed. Saepe, vitae id?
-            </Typography>
+          </Box>
+          <Typography variant="h2" component="h1" fontWeight={600}>
+            Healthier Hearts
+          </Typography>
+          <Typography variant="h2" component="h1" fontWeight={600}>
+            Come From
+          </Typography>
+          <Typography
+            variant="h2"
+            component="h1"
+            fontWeight={600}
+            color="primary.main"
+          >
+            Preventive Care
+          </Typography>
+          <Typography sx={{ my: 4 }}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit eum
+            iusto consequatur eius, doloribus nesciunt facere aliquid eveniet
+            et. Rerum maiores saepe cupiditate repellat recusandae atque sed.
+            Saepe, vitae id?
+          </Typography>
+          <Box sx={{ display: "flex", gap: 2 }}>
+            <Button variant="contained">Make appointment</Button>
+            <Button variant="outlined">Contact us</Button>
           </Box>
         </Box>
-        <Box>right</Box>
+        <Box
+          sx={{
+            p: 1,
+            flex: 1,
+            display: "flex",
+            justifyContent: "center",
+            position: "relative",
+            mt: 0,
+          }}
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              left: "200px",
+              top: "-30px",
+            }}
+          >
+            <Image
+              src={assets.svgs.arrow}
+              width={100}
+              height={100}
+              alt="arrow"
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              gap: 2,
+            }}
+          >
+            <Box mt={4}>
+              <Image
+                src={assets.images.doctor1}
+                width={240}
+                height={380}
+                alt="doctor1"
+              />
+            </Box>
+            <Box>
+              <Image
+                src={assets.images.doctor2}
+                width={240}
+                height={350}
+                alt="doctor2"
+              />
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              position: "absolute",
+              top: "220px",
+              left: "150px",
+            }}
+          >
+            <Image
+              src={assets.images.doctor3}
+              width={240}
+              height={240}
+              alt="doctor3"
+            />
+          </Box>
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "-50px",
+              right: 0,
+              zIndex: "-1",
+            }}
+          >
+            <Image
+              src={assets.images.stethoscope}
+              width={180}
+              height={180}
+              alt="doctor3"
+            />
+          </Box>
+        </Box>
       </Container>
     </>
   );
