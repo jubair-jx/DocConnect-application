101c2c8b5a56dcd027fa94f9611c72c398638285
jubair-jx <jubair.official97@gmail.com>
2024-05-03 22:23:35 +0600

    specialites is working now

--- DocConnect-Backend/src/app/modules/Specialties/specialties.route.ts
+++ DocConnect-Backend/src/app/modules/Specialties/specialties.route.ts
@@ -1,7 +1,5 @@
-import { UserRole } from "@prisma/client";
 import { NextFunction, Request, Response, Router } from "express";
 import { fileUploader } from "../../../helpers/sendUploader";
-import auth from "../../middlewares/auth";
 import { specialitesValidation } from "./specialites.validation";
 import { specialitesControllers } from "./specialties.controller";
 
@@ -9,7 +7,7 @@ const specialitesRoutes = Router();
 
 specialitesRoutes.post(
   "/",
-  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  // auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   fileUploader.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = specialitesValidation.createSpecialitesValidation.parse(
--- PH-Healthcare-Frontend/src/app/(withCommonLayout)/page.tsx
+++ PH-Healthcare-Frontend/src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/ui/HomePage/HeroSection";
+import { Speacialist } from "@/components/ui/HomePage/Speacialist";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
+      <Speacialist />
     </>
   );
 }
