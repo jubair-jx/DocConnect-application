668cbecb9f2af27b8fb2d1398b3b79cba5f8a968
jubair-jx <jubair.official97@gmail.com>
2024-05-04 11:58:32 +0600

    why choose section is completed now

--- PH-Healthcare-Frontend/src/app/(withCommonLayout)/page.tsx
+++ PH-Healthcare-Frontend/src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from "@/components/ui/HomePage/HeroSection";
 import { Speacialist } from "@/components/ui/HomePage/Speacialist";
 import { TopRatedDoctor } from "@/components/ui/HomePage/TopRatedDoctor";
+import WhyUs from "@/components/ui/HomePage/WhyUs";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <HeroSection />
       <Speacialist />
       <TopRatedDoctor />
+      <WhyUs />
     </>
   );
 }
