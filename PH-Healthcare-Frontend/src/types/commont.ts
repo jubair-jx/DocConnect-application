5d2d769627ba20d42e3d4264f2e632fb9418e700
jubair-jx <jubair.official97@gmail.com>
2024-05-07 23:06:26 +0600

    dashboard icon is added now

--- PH-Healthcare-Frontend/src/types/commont.ts
+++ PH-Healthcare-Frontend/src/types/commont.ts
@@ -1,9 +1,20 @@
 import { USER_ROLE } from "@/constant/role";
+import { SvgIconTypeMap } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 
 export type TMeta = {
   page: number;
   limit: number;
   total: number;
 };
+
+export interface TDrawerItem {
+  title: string;
+  path: string;
+  parentPath?: string;
+  icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> & { muiName: string };
+
+  child?: TDrawerItem[];
+}
 
 export type UserRole = keyof typeof USER_ROLE;
--- PH-Healthcare-Frontend/src/utils/DrawerItems.ts
+++ PH-Healthcare-Frontend/src/utils/DrawerItems.ts
@@ -1,5 +1,26 @@
-import { UserRole } from "@/types";
+import { USER_ROLE } from "@/constant/role";
+import { TDrawerItem, UserRole } from "@/types";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+export const GenerateDrawerItems = (role: UserRole): TDrawerItem[] => {
+  const rolesMenus: TDrawerItem[] = [];
 
-export const DrawerItems = (role: UserRole) => {
-  return [];
+  switch (role) {
+    case USER_ROLE.SUPER_ADMIN:
+      rolesMenus.push(
+        {
+          title: "Dashboard",
+          path: `${role}`,
+          icon: DashboardIcon,
+        },
+        {
+          title: "Manage Users",
+          path: `${role}/manage-users`,
+          icon: ManageAccountsIcon,
+        }
+      );
+      break;
+  }
+
+  return [...rolesMenus];
 };
