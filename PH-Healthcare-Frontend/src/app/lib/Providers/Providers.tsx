646fa1509131e024b27a98b4b8954475bb22a7ff
jubair-jx <jubair.official97@gmail.com>
2024-05-07 11:56:00 +0600

    redux added now

--- PH-Healthcare-Frontend/src/app/lib/Providers/Providers.tsx
+++ PH-Healthcare-Frontend/src/app/lib/Providers/Providers.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { ThemeProvider } from "@emotion/react";
+import { Provider } from "react-redux";
 import theme from "../theme/theme";
+import { store } from "@/redux/store";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Provider>
+  );
 }
---
+++ PH-Healthcare-Frontend/src/redux/api/baseApi.ts
@@ -0,0 +1,14 @@
+// Need to use the React-specific entry point to import createApi
+import { axiosBaseQuery } from "@/axios/axiosBaseQuery ";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  endpoints: (builder) => ({}),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const {} = baseApi;
