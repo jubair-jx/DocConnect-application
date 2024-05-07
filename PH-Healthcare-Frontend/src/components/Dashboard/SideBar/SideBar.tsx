c66bbec893995041ce7b01fae5a90b350693c2a1
jubair-jx <jubair.official97@gmail.com>
2024-05-07 21:38:55 +0600

    dashboard part is working now

---
+++ PH-Healthcare-Frontend/src/components/Dashboard/SideBar/SideBar.tsx
@@ -0,0 +1,71 @@
+import assets from "@/assets";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import { Box, Divider, Stack, Typography } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Image from "next/image";
+import Link from "next/link";
+function SideBar() {
+  const drawer = (
+    <div>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+  return (
+    <Box>
+      <Stack
+        sx={{
+          py: 1,
+          mt: 1,
+        }}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        gap={1}
+        component={Link}
+        href="/"
+      >
+        <Image src={assets.svgs.logo} width={40} height={40} alt="logo" />
+        <Typography variant="h5" component={Link} href={"/"} fontWeight={600}>
+          <Box component={"span"} color={"primary.main"}>
+            Doc
+          </Box>
+          {""}
+          Connect
+        </Typography>
+      </Stack>
+      <Divider />
+      {drawer}
+    </Box>
+  );
+}
+
+export default SideBar;
