428d325d92be0420190c84024e279401ed219a8f
jubair-jx <jubair.official97@gmail.com>
2024-05-07 23:18:04 +0600

    users infromation icon and data has been added now

--- PH-Healthcare-Frontend/src/utils/DrawerItems.ts
+++ PH-Healthcare-Frontend/src/utils/DrawerItems.ts
@@ -1,13 +1,20 @@
 import { USER_ROLE } from "@/constant/role";
 import { TDrawerItem, UserRole } from "@/types";
+import AddCardIcon from '@mui/icons-material/AddCard';
+import BookOnlineIcon from "@mui/icons-material/BookOnline";
+import BrowseGalleryIcon from "@mui/icons-material/BrowseGallery";
 import DashboardIcon from "@mui/icons-material/Dashboard";
+import FolderSpecialIcon from "@mui/icons-material/FolderSpecial";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
+import MedicationIcon from '@mui/icons-material/Medication';
+import RateReviewIcon from "@mui/icons-material/RateReview";
 export const GenerateDrawerItems = (role: UserRole): TDrawerItem[] => {
-  const rolesMenus: TDrawerItem[] = [];
+  const roleMenus: TDrawerItem[] = [];
 
   switch (role) {
     case USER_ROLE.SUPER_ADMIN:
-      rolesMenus.push(
+      roleMenus.push(
         {
           title: "Dashboard",
           path: `${role}`,
@@ -20,7 +27,85 @@ export const GenerateDrawerItems = (role: UserRole): TDrawerItem[] => {
         }
       );
       break;
+
+    case USER_ROLE.ADMIN:
+      roleMenus.push(
+        {
+          title: "Dashboard",
+          path: `${role}`,
+          icon: DashboardIcon,
+        },
+        {
+          title: "Specialties",
+          path: `${role}/specialties`,
+          icon: FolderSpecialIcon,
+        },
+        {
+          title: "Doctors",
+          path: `${role}/doctors`,
+          icon: MedicalInformationIcon,
+        },
+        {
+          title: "Schedules",
+          path: `${role}/schedules`,
+          icon: BrowseGalleryIcon,
+        },
+        {
+          title: "Appointments",
+          path: `${role}/appointments`,
+          icon: BookOnlineIcon,
+        },
+        {
+          title: "Reviews",
+          path: `${role}/reviews`,
+          icon: RateReviewIcon,
+        }
+      );
+      break;
+
+    case USER_ROLE.DOCTOR:
+      roleMenus.push(
+        {
+          title: "Dashboard",
+          path: `${role}`,
+          icon: DashboardIcon,
+        },
+        {
+          title: "Schedules",
+          path: `${role}/schedules`,
+          icon: BrowseGalleryIcon,
+        },
+        {
+          title: "Appointments",
+          path: `${role}/appointment`,
+          icon: BookOnlineIcon,
+        }
+      );
+      break;
+
+    case USER_ROLE.PATIENT:
+      roleMenus.push(
+        {
+          title: "Appointments",
+          path: `${role}/appointments`,
+          icon: DashboardIcon,
+        },
+        {
+          title: "Prescriptions",
+          path: `${role}/prescriptions`,
+          icon: MedicationIcon,
+        },
+        {
+          title: "Payment History",
+          path: `${role}/payment-history`,
+          icon: AddCardIcon ,
+        }
+      );
+      break;
+
+    default:
+      break;
   }
 
-  return [...rolesMenus];
+  return [...roleMenus];
 };
