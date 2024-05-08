2f5a194c504df55a0af045fbd485523d4117b12a
jubair-jx <jubair.official97@gmail.com>
2024-05-08 23:49:48 +0600

    reusable form has been made it

--- PH-Healthcare-Frontend/src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ PH-Healthcare-Frontend/src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,5 +1,18 @@
+import RUModal from "@/components/shared/Modal/RUModal";
+import { Stack, TextField } from "@mui/material";
+
 const SpecialtiesPage = () => {
-  return <div>SpecialtiesPage</div>;
+  // onClick={() => setIsModalOpen(true)}
+  return (
+    <Stack direction="row" justifyContent="space-between" alignItems="center">
+      <button className="px-3 py-2 text-sm rounded-md bg-blue-500 text-white">
+        Create Specialty
+      </button>
+      <RUModal />
+      {/* <SpecialtyModal open={isModalOpen} setOpen={setIsModalOpen} /> */}
+      <TextField size="small" placeholder="Search Specialist" />
+    </Stack>
+  );
 };
 
 export default SpecialtiesPage;
