c82e8fbd0ccd59e0175b2d41ecd8a6eebc696c56
jubair-jx <jubair.official97@gmail.com>
2024-05-09 01:24:45 +0600

    form data and file uploaded using rtk

--- PH-Healthcare-Frontend/src/app/(withDashboardLayout)/dashboard/admin/specialties/components/schedulesModal.tsx
+++ PH-Healthcare-Frontend/src/app/(withDashboardLayout)/dashboard/admin/specialties/components/schedulesModal.tsx
@@ -2,6 +2,7 @@ import RUFileUploader from "@/components/Form/FileUploader/RUFileUploader";
 import PHForm from "@/components/Form/PHFrom";
 import PHInput from "@/components/Form/PHInput";
 import RUModal from "@/components/shared/Modal/RUModal";
+import { modifyPayload } from "@/utils/FormDataPayload";
 import { Grid } from "@mui/material";
 import React from "react";
 import { FieldValues } from "react-hook-form";
@@ -12,7 +13,13 @@ type TProps = {
 };
 
 const SchedulesModal = ({ open, setOpen }: TProps) => {
-  const handleFormSubmit = (values: FieldValues) => {};
+  const handleFormSubmit = (values: FieldValues) => {
+    const data = modifyPayload(values);
+    try {
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
   return (
     <>
       <RUModal open={open} setOpen={setOpen} title={"Create Specialites"}>
