02f57d86b27697de9b16732fec64a615c5ea2d63
jubair-jx <jubair.official97@gmail.com>
2024-05-09 12:35:25 +0600

    Delete functionlity implement soon

--- PH-Healthcare-Frontend/src/app/(withDashboardLayout)/dashboard/admin/specialties/components/DataTable.tsx
+++ PH-Healthcare-Frontend/src/app/(withDashboardLayout)/dashboard/admin/specialties/components/DataTable.tsx
@@ -1,13 +1,41 @@
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { Box, IconButton } from "@mui/material";
+import { DataGrid, GridColDef, GridDeleteIcon } from "@mui/x-data-grid";
+import Image from "next/image";
+
+const handleDelete = (id: string) => {
+  console.log(id);
+};
 
 const columns: GridColDef[] = [
-  { field: "title", headerName: "title", width: 200 },
+  { field: "title", headerName: "title", width: 300 },
   {
     field: "icon",
     headerName: "icon",
+    width: 300,
+    renderCell: ({ row }) => {
+      return (
+        <Box my={1}>
+          <Image
+            className="rounded-md"
+            src={row.icon}
+            width={30}
+            height={30}
+            alt="icon"
+          />
+        </Box>
+      );
+    },
+  },
+  {
+    field: "Action",
+    headerName: "Action",
     width: 100,
     renderCell: ({ row }) => {
-      return console.log(row);
+      return (
+        <IconButton onClick={() => handleDelete(row.id)} aria-label="delete">
+          <GridDeleteIcon />
+        </IconButton>
+      );
     },
   },
 ];
